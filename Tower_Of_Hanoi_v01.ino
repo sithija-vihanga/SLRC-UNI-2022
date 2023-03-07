dfb576e436ad4e94d96da9e2d29bbd678df48f7b
Sithija Vihanga Ranaraja <116638289+sithija-vihanga@users.noreply.github.com>
2023-03-08 00:34:27 +0530

    Add files via upload

---
+++ Tower_Of_Hanoi_v01.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void towerOfHanoi(int num1, int num2){
+     ///////////////// SCAN FOR THE BOXES ///////////////////////////
+     
+     //Follow the line turn 90 degrees left at first function: Go forward : scan the color : turn 180 degrees: Go forward: turn left 90 degrees
+    // Repeat 3 times
+
+    ////////////////////////////////////////////////////////////////
+
+    /////////////////MAKING FIRST TOWER ////////////////////////////
+
+     //bring red color box to num1: green-> blue
+
+     ///////////////////////////////////////////////////////////////
+
+     ///////////////// TOWER OF HANOI SOLUTION ////////////////////
+     
+     //generate which box is to place in which square
+     //Generate a path to go from start to end.
+     // Move the boxes in the given order
+     
+  }
